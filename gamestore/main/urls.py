3367c492aa73bc77823774e2592bb8708ec54dc0
barney <brnbsjm@gmail.com>
2019-01-15 18:57:23 +0300

    Add views for user authentication setup AuthForm class

--- gamestore/main/urls.py
+++ gamestore/main/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
 from . import views
+from .forms import AuthenticationForm
 
 urlpatterns = [
   path(r'', views.index, name='index'), # First param '', url to match, 2nd is func to be executed, 3rd name sets url name
+  path(r'accounts/login', login, {
+    'template_name': './login.html',
+    'authentication_form': AuthenticationForm
+  }, name='login'),
+  path(r'accounts/logout', logout, {
+    'next_page': '/'
+  },  name='logout'),
 ]
